9e548c2967bcae9349edd37a03e78647bad6bdf3
Tobias Fechner <tobias.fechner@hotmail.co.uk>
2025-05-13 15:55:00 +0200

    Amend ignore message conditions

--- src/app/api/story/webhook/route.ts
+++ src/app/api/story/webhook/route.ts
@@ -19,19 +19,19 @@ export async function POST(request: NextRequest) {
 
     const topicName = data.message?.reply_to_message?.forum_topic_created?.name;
 
-    if (!(data.message?.chat?.type === 'private') && (!topicName || !topicName.includes('_bot') || !topicName.includes('prisma_events_storying'))) {
+    if ((data.message?.chat?.type === 'private') || (topicName?.includes('_bot') || topicName?.includes('prisma_events_storying'))) {
+      await redis.lpush('telegram_messages', JSON.stringify(data));
+      logger.info(`Message queued. chat ID: ${chatId}, message ID: ${messageId} `);
+  
+      // Send silent reply
+      await setMessageReaction(chatId, messageId);
+      logger.info('⚡ Message reacted to.');
+  
+      return NextResponse.json({ status: 'ok' });
+    } else {
       logger.info('Message ignored.');
       return NextResponse.json({ status: 'ignored' });
     }
-
-    await redis.lpush('telegram_messages', JSON.stringify(data));
-    logger.info(`Message queued. chat ID: ${chatId}, message ID: ${messageId} `);
-
-    // Send silent reply
-    await setMessageReaction(chatId, messageId);
-    logger.info('⚡ Message reacted to.');
-
-    return NextResponse.json({ status: 'ok' });
   } catch (error) {
     logger.error('Webhook error', { error });
     return handleError(error);
